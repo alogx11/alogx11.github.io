4efa409a84bb63f9084ddcb45ca9a62e0d952356
alogx11 <alogx11@gmail.com>
2022-10-28 04:00:55 -0500

    poly world added

--- main.js
+++ main.js
@@ -1,52 +1,65 @@
-import * as THREE from 'https://unpkg.com/three@0.145/build/three.module.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {GUI} from 'dat.gui';
+import {MapControls, OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module'
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight, 1, 1000);
-  const renderer = new THREE.WebGLRenderer();
 
-  console.log(scene);
-  console.log(camera);
-  console.log(renderer);
-
-renderer.setSize(innerWidth, innerHeight);
-renderer.setPixelRatio(devicePixelRatio);
-  document.body.appendChild(renderer.domElement);
-
-  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({color: 0x00FF00});
-  const mesh = new THREE.Mesh(boxGeometry, material);
-  
-  scene.add(mesh);
-
-  camera.position.z = 5;
+  const { scene, renderer, camera, gui, stats } = setUp();
 
   //create array with strings of sound file names
   const audioFile = ["A4.mp3", "A5.mp3", "C4.mp3", "C5.mp3", "D4.mp3",
    "D5.mp3", "E4.mp3", "E5.mp3", "G4.mp3", "G5.mp3"];
 
+   const gltfLoader = new GLTFLoader();
+   gltfLoader.load('/assets/lowpolyworld.gltf', function( polyWorld ) {
+     
+     
+     scene.add(polyWorld.scene);
+     polyWorld.scale.set(.1);
+     //polyWorld.poosition.x = 0;
+     //polyWorld.poosition.y = 0;
+     //polyWorld.poosition.z = 0;
+
+   }); 
+   
+   
+
   animate();
-mousePresssed();
-loadGLTF();
+  mousePresssed();
+
+function setUp() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 1, 1000);
+
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize(innerWidth, innerHeight);
+  renderer.setPixelRatio(devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+
+  const ambientLight = new THREE.AmbientLight(0xFFFFFF, 5);
+  ambientLight.castShadow = true;
+  scene.add(ambientLight);
+
+  const gui = new GUI()
+  const controls = new OrbitControls(camera, renderer.domElement);
+  const stats = Stats();
+  document.body.appendChild(stats.dom);
+
+  return { scene, renderer, camera, gui, controls, stats };
+}
 
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02
-  }
-  function loadGLTF() {
-    let polyWorld = new THREE.loadGLTF();
-
-    polyWorld.load('/assets/lowpolyworld.gltf', (gltf) => {
-      polyWorldMesh = gltf.scene
-      polyWorldMesh.scale.set(1);
-      scene.add(polyWorldMesh);
-      polyWorldMesh.poosition.x = 0;
-      polyWorldMesh.poosition.y = 0;
-      polyWorldMesh.poosition.z = 0;
-
-    });
-
+    //mesh.rotation.x += 0.01;
+    //mesh.rotation.y += 0.02
+    stats.update();
+    controls.update();
+    
   }
 
   function playAudio(){
@@ -65,9 +78,6 @@ loadGLTF();
   }
 
   function randomItem(arr){
-    //array of soundFileNames
-    const audioFile = ["A4.mp3", "A5.mp3", "C4.mp3", "C5.mp3", "D4.mp3",
-   "D5.mp3", "E4.mp3", "E5.mp3", "G4.mp3", "G5.mp3"];
     //random index value
     const randomIndex = Math.floor(Math.random() * arr.length);
     // get random item
